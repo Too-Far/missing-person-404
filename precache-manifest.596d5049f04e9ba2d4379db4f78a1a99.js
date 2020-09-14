2c1eefab433d52d5ea43c20e0b9e873d6dda182d
Ryan <rdturner40@gmail.com>
2020-09-14 03:58:22 -0400

    Updates

---
+++ precache-manifest.596d5049f04e9ba2d4379db4f78a1a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b774dcd0f0b2814c9ddbf5dfab711136",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3b8c00e70e45f4d3ca4",
+    "url": "./static/css/main.91d6e2ec.chunk.css"
+  },
+  {
+    "revision": "8f26f79129d327b354bc",
+    "url": "./static/js/2.a4bf4c78.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.a4bf4c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3b8c00e70e45f4d3ca4",
+    "url": "./static/js/main.0d19ca44.chunk.js"
+  },
+  {
+    "revision": "bdddde459d9e7c451d92",
+    "url": "./static/js/runtime-main.d363b0ac.js"
+  }
+]);
